0b0d05e9e7129321e86315bf0858bda44245d749
Glenn Sarti <glenn.sarti@outlook.com>
2015-05-03 20:51:08 +0800

    Updating tests for the new module test syntax

--- posh-neo-cm/tests/unit/Confirm-Neo4jHome.Tests.ps1
+++ posh-neo-cm/tests/unit/Confirm-Neo4jHome.Tests.ps1
@@ -2,29 +2,33 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 $common = Join-Path (Split-Path -Parent $here) 'Common.ps1'
 . $common
-. "$src\$sut"
 
-Describe "Confirm-Neo4jHome" {
-  Context "Invalid Neo4jHome path" {
-    It "return false for missing parameter" {
-      Confirm-Neo4jHome | Should Be $false
-    }
-    It "return false for a missing directory" {
-      Confirm-Neo4jHome -Neo4jHome 'TestDrive:\Some-dir-that-doesnt-exist' | Should Be $false
-    }
-  }
+Import-Module "$src\Neo4j-Management.psm1"
 
-  Context "Valid Neo4jHome path" {
-    New-MockNeo4jInstall -NeoVersion '99.99' -IsEnterpriseServer
-
-    It "return true" {
-      Confirm-Neo4jHome -Neo4jHome (Get-MockNeo4jInstall) | Should Be $true
+InModuleScope Neo4j-Management {
+  Describe "Confirm-Neo4jHome" {
+    Context "Invalid Neo4jHome path" {
+      It "return false for missing parameter" {
+        Confirm-Neo4jHome | Should Be $false
+      }
+      It "return false for a missing directory" {
+        Confirm-Neo4jHome -Neo4jHome 'TestDrive:\Some-dir-that-doesnt-exist' | Should Be $false
+      }
     }
-    It "return true for alias Home" {
-      Confirm-Neo4jHome -Home (Get-MockNeo4jInstall) | Should Be $true
-    }
-    It "return true for piped input" {
-      { (Get-MockNeo4jInstall) | Confirm-Neo4jHome } | Should Be $true
+  
+    Context "Valid Neo4jHome path" {
+      $neo4jPath = 'TestDrive:\SomePath'
+      Mock Test-Path { $true } -ParameterFilter { $Path -eq $neo4jPath }
+  
+      It "return true" {
+        Confirm-Neo4jHome -Neo4jHome $neo4jPath | Should Be $true
+      }
+      It "return true for alias Home" {
+        Confirm-Neo4jHome -Home $neo4jPath | Should Be $true
+      }
+      It "return true for piped input" {
+        { $neo4jPath | Confirm-Neo4jHome } | Should Be $true
+      }
     }
   }
 }
--- posh-neo-cm/tests/unit/Confirm-Neo4jServerObject.Tests.ps1
+++ posh-neo-cm/tests/unit/Confirm-Neo4jServerObject.Tests.ps1
@@ -2,73 +2,77 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 $common = Join-Path (Split-Path -Parent $here) 'Common.ps1'
 . $common
-. "$src\$sut"
 
-Describe "Confirm-Neo4jServerObject" {
-  Context "Invalid Server Object" {
-    It "throw if missing object" {
-      { Confirm-Neo4jServerObject } | Should Throw
+Import-Module "$src\Neo4j-Management.psm1"
+
+InModuleScope Neo4j-Management {
+  Describe "Confirm-Neo4jServerObject" {
+    Context "Invalid Server Object" {
+      It "throw if missing object" {
+        { Confirm-Neo4jServerObject } | Should Throw
+      }
+      It "return false if missing all properties" {
+        (Confirm-Neo4jServerObject -Neo4jServer (New-Object -TypeName PSCustomObject)) | Should be $false
+      }
+      It "return false if Home is null" {
+        $serverObject = New-Object -TypeName PSCustomObject -Property @{
+          'Home' = $null;
+          'ServerVersion' = '99.99';
+          'ServerType' = 'Community';
+        }
+        (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
+      }
+      It "return false if ServerVersion is null" {
+        $serverObject = New-Object -TypeName PSCustomObject -Property @{
+          'Home' = 'TestDrive:\SomePath';
+          'ServerVersion' = $null;
+          'ServerType' = 'Community';
+        }
+        (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
+      }
+      It "return false if ServerType is null" {
+        $serverObject = New-Object -TypeName PSCustomObject -Property @{
+          'Home' = 'TestDrive:\SomePath';
+          'ServerVersion' = '99.99';
+          'ServerType' = $null;
+        }
+        (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
+      }
+      It "return false if ServerType is not Community or Enterprise" {
+        $serverObject = New-Object -TypeName PSCustomObject -Property @{
+          'Home' = 'TestDrive:\SomePath';
+          'ServerVersion' = '99.99';
+          'ServerType' = 'SomethingSilly';
+        }
+        (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
+      }
+      It "return false if Home does not exist" {
+        $serverObject = New-Object -TypeName PSCustomObject -Property @{
+          'Home' = 'TestDrive:\SomePath';
+          'ServerVersion' = '99.99';
+          'ServerType' = 'Community';
+        }
+        (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
+      }
     }
-    It "return false if missing all properties" {
-      (Confirm-Neo4jServerObject -Neo4jServer (New-Object -TypeName PSCustomObject)) | Should be $false
-    }
-    It "return false if Home is null" {
+  
+    Context "Valid Server Object" {
+      # Setup
+      $neo4jPath = 'TestDrive:\Neo4j'
+      Mock Test-Path { $true }  -ParameterFilter { $Path -eq $neo4jPath }
       $serverObject = New-Object -TypeName PSCustomObject -Property @{
-        'Home' = $null;
+        'Home' = $neo4jPath;
         'ServerVersion' = '99.99';
         'ServerType' = 'Community';
       }
-      (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
-    }
-    It "return false if ServerVersion is null" {
-      $serverObject = New-Object -TypeName PSCustomObject -Property @{
-        'Home' = Get-MockNeo4jInstall;
-        'ServerVersion' = $null;
-        'ServerType' = 'Community';
+      $result = Confirm-Neo4jServerObject -Neo4jServer $serverObject
+  
+      It "returns true" {
+        $result | Should be $true
       }
-      (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
-    }
-    It "return false if ServerType is null" {
-      $serverObject = New-Object -TypeName PSCustomObject -Property @{
-        'Home' = Get-MockNeo4jInstall;
-        'ServerVersion' = '99.99';
-        'ServerType' = $null;
+      It "attemtps to validate the path" {
+        Assert-MockCalled Test-Path -Times 1
       }
-      (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
-    }
-    It "return false if ServerType is not Community or Enterprise" {
-      $serverObject = New-Object -TypeName PSCustomObject -Property @{
-        'Home' = Get-MockNeo4jInstall;
-        'ServerVersion' = '99.99';
-        'ServerType' = 'SomethingSilly';
-      }
-      (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
-    }
-    It "return false if Home does not exist" {
-      $serverObject = New-Object -TypeName PSCustomObject -Property @{
-        'Home' = 'TestDrive:\Some-directory-that-doesnt-exist';
-        'ServerVersion' = '99.99';
-        'ServerType' = 'Community';
-      }
-      (Confirm-Neo4jServerObject -Neo4jServer $serverObject) | Should be $false
-    }
-  }
-
-  Context "Valid Server Object" {
-    # Setup
-    Mock Test-Path { return $true }
-    $serverObject = New-Object -TypeName PSCustomObject -Property @{
-      'Home' = Get-MockNeo4jInstall;
-      'ServerVersion' = '99.99';
-      'ServerType' = 'Community';
-    }
-    $result = Confirm-Neo4jServerObject -Neo4jServer $serverObject
-
-    It "returns true" {
-      $result | Should be $true
-    }
-    It "attemtps to validate the path" {
-      Assert-MockCalled Test-Path -Times 1
     }
   }
 }
