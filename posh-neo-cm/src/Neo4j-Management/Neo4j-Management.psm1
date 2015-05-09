ce1cb94fb45522f4ec2211c72417d11bf464716a
Glenn Sarti <glenn.sarti@outlook.com>
2015-05-09 20:40:49 +0800

    Unblock-File is PS 3.0 and not required when cloning from github.

--- posh-neo-cm/src/Neo4j-Management/Neo4j-Management.psm1
+++ posh-neo-cm/src/Neo4j-Management/Neo4j-Management.psm1
@@ -1,5 +1,4 @@
 # Import this modules functions etc.
-Get-ChildItem -Path $PSScriptRoot | Unblock-File
 Get-ChildItem -Path $PSScriptRoot\*.ps1 | ForEach-Object {
 Write-Verbose "Importing $($_.Name)..."
 . ($_.Fullname)
