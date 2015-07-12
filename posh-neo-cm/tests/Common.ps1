12c0dddccf70b35654888405399bcb4cd7dcc08d
Glenn Sarti <glennsarti@users.noreply.github.com>
2015-07-12 21:14:07 +0800

    Made the folder structure mirror that of Neo4j

--- posh-neo-cm/tests/Common.ps1
+++
@@ -1,5 +0,0 @@
-# WARNING this should be copied directly into real repo
-$DebugPreference = "SilentlyContinue"
-
-$here = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$src = Join-Path -Path (Join-Path -Path (Split-Path $here) -ChildPath 'src') -ChildPath 'Neo4j-Management'
