42454a501c39dd9e252690d8ae3f0870af009dc9
Glenn Sarti <glennsarti@users.noreply.github.com>
2015-08-03 21:01:43 +0800

    Updated pester batch file for new source location

--- posh-neo-cm/tools/RunPester.bat
+++ posh-neo-cm/tools/RunPester.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-PUSHD "%~dp0..\tests"
+PUSHD "%~dp0..\packaging\standalone\src\tests\Neo4j-Management"
 
 @PowerShell -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ^
- "& Import-Module '..\pester\Pester.psm1';  & { Invoke-Pester -Strict -EnableExit %*}"
+ "& Import-Module '%~dp0..\pester\Pester.psd1';  & { Invoke-Pester -Strict -EnableExit %*}"
 
 POPD
 
